50059f7963cd4a5768679a68aa4aedc935d55215
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2025-12-07 12:10:21 +0530

    remove REDIS_USERNAME as we use default

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ const io = new Server(server, {
 const redis = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
   port: process.env.REDIS_PORT || 6379,
-  username: process.env.REDIS_USERNAME || '',
   password: process.env.REDIS_PASSWORD || '',
   retryStrategy: (times) => {
     const delay = Math.min(times * 50, 2000);
